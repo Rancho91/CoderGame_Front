f95dd3518a5a04bf9a3afd07bffdbf9ab771161e
Rancho91 <ramirosanchezsolano@gmail.com>
2023-05-16 12:06:49 -0300

    home y slider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route/>
+      </Routes>
     </div>
   );
 }
---
+++ src/component/Carrousel/imagecss.js
@@ -0,0 +1,11 @@
+export default imageCSS = (image) =>{
+    const imagenCSS = {
+        backgroundImage: image,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        width: '400px',
+        height: '300px',
+      }
+      return imageCSS
+}
