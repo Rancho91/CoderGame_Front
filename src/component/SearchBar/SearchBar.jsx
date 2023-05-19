fb60a8714770837e33a629a9eab0e331e17b67d9
walterporta <walterandresporta@gmail.com>
2023-05-18 23:29:17 -0300

    lupa al searchBar

--- src/component/SearchBar/SearchBar.jsx
+++ src/component/SearchBar/SearchBar.jsx
@@ -1,30 +1,30 @@
-import {getGameByName} from '../../redux/actions/actions';
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
-const { useState } = require("react");
-const { useDispatch } = require("react-redux");
+const SearchBar = ({ handlerFilter }) => {
+  const dispatch = useDispatch();
+  const [name, setName] = useState("");
 
+  const handleInput = (e) => {
+    e.preventDefault();
+    setName(e.target.value);
+  };
 
-const SearchBar = ({handlerFilter}) => {
-    const dispatch  = useDispatch();
-    const [name, setName] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handlerFilter(e); 
+  };
 
-    const handleInput = (e) => {
-        e.preventDefault();
-        setName(e.target.value);
-    }
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        handlerFilter(e)
-    }
-    
-    return(
-        <div>
-            <input type="text" className="btn btn-dark" placeholder="Search" onChange={handleInput} />
-            <button type="submit" name="name" value={name} className="btn btn-secondary" onClick={handleSubmit} >Search</button>
-        </div>
-    )
-
-}
+  return (
+    <div>
+      <input type="text" className="btn btn-dark" placeholder="What game are you looking for?" onChange={handleInput} />
+      <button type="submit" name="name" value={name} className="btn btn-secondary" onClick={handleSubmit}>
+        <FontAwesomeIcon icon={faSearch} />
+      </button>
+    </div>
+  );
+};
 
 export default SearchBar;
