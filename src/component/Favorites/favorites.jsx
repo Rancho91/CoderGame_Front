9a04aa3228c6a45c133ec15631eefe078573d9e8
Rancho91 <ramirosanchezsolano@gmail.com>
2023-06-06 09:46:46 -0300

    Favorites, logica en detail

--- src/component/Favorites/favorites.jsx
+++ src/component/Favorites/favorites.jsx
@@ -1,61 +1,3 @@
-// import React, { useEffect,useState } from "react";
-// import axios from "axios";
-// import Card from "../card/card";
-// import { useAuth0 } from "@auth0/auth0-react";
-// import Login from "../NavBar/login/login"
-// function Favorites (){
-//     const [listFavorites, setListFavorites]= useState([])
-//     const {user, isAuthenticated, loginWithRedirect} = useAuth0()
-//     const [refresh, setRefresh] = useState(true)
-
-//     const refreshHandler = () =>{
-//         setRefresh(!refresh)
-//       }
-//     useEffect(()=>{
-//         const getFavorites = async () =>{
-//             if(isAuthenticated){
-//                 try {       
-//                         const response = await axios.get(`http://localhost:3001/user/favorites/${user?.sub}`)
-//                         setListFavorites(response.data.listFavorites)
-//                                     }
-  
-//                  catch (error) {
-//                  window.alert(error.message)   
-//         }}
-//         }
-//          getFavorites()
-//     },[refresh])
-// console.log(listFavorites)
-//     return(
-//     <div className="container">
-//         <div className="row">
-//             {listFavorites?.map((game)=>{
-//                 game = {...game, Favorites:[{"buy": false}]}
-//                return( 
-//                <div className="col-sm-12 col-md-3">
-//                 <Card game={game} refreshHandler={refreshHandler}/>
-
-//                </div>
-//             )})}
-//          <div>
-//             {isAuthenticated?null:<Login rute="http://localhost:3000/favorites"/>}
-//          </div>
-//         </div>
-    
-    
-    
-    
-//     </div>
-
-//     )
-// }
-
-// export default Favorites
-
-
-
-
-
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Card from "../card/card";
@@ -69,7 +11,7 @@ function Favorites() {
   const [refresh, setRefresh] = useState(true);
 
   const refreshHandler = () => {
-    // setRefresh(!refresh);
+   setRefresh(!refresh);
   };
 
   useEffect(() => {
--- src/component/card/card.jsx
+++ src/component/card/card.jsx
@@ -1,82 +1,3 @@
-// import React,{useState} from "react";
-// import { Link } from "react-router-dom";
-// import styles from "./card.module.css"
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faHeart} from "@fortawesome/free-solid-svg-icons";
-// import axios from "axios";
-// import { useAuth0 } from "@auth0/auth0-react";
-
-// function Card ({game, refreshHandler}){
-//  const {name, image, description, price, released,Favorites, id} = game
-//  const {user} = useAuth0()
-
-//  const addDleneteFavorites= async()=>{
-
-//     if(!Favorites.length){
-//       try {
-//         await axios.post("http://localhost:3001/user/favorites",{idUser:user.sub, idVideogame:id})
-//         refreshHandler()
-//           } catch (error) {
-//         window.alert(error.message)
-//       }
-//     } else{
-//       try {
-//         await axios.put("http://localhost:3001/user/favorites",{idUser:user.sub, idVideogame:id})
-//         refreshHandler()
-//       } catch (error) {
-//         window.alert(error.message)
-//       }
-//     }
-//   }
-
-//     return(
-//       <div className={`col-sm-12 col-md-12 text-center ${styles.container}`}>
-//       <div className={`row ${styles.title}`}>
-//         <div className={`${styles.price}`}>
-//           <p className="mt-3 mr-3">{price}</p>
-//         </div>
-//         <div className="col-12">
-//           <h1>{name}</h1>
-//         </div>
-//       </div>
-//         <Link to={`/videogames/${id}`}>
-
-//       <div  className={`row ${styles.image}`} style={{ backgroundImage: `url(${image})` }} >
-//       </div>
-//       </Link>
-
-//       {/* <div className="row">
-//         <div className="col-12 d-flex align-items-center flex-column">
-//           <p className={styles.description}>{released}</p>
-//         </div>
-//       </div> */}
-
-//       {/* <div className="row">
-//         <div className="col-12 d-flex align-items-center">
-//           <p className={styles.description}>{description?description:null}</p>
-//         </div>
-//       </div> */}
-
-//       <div className="row">
-//         <div className={`col-6 ${styles.buy}`}>
-//         <button onClick={addDleneteFavorites}>
-//             <FontAwesomeIcon
-//             icon={faHeart}
-//             className={Favorites?.length?styles.heartIconFav:styles.heartIcon}
-//             />
-//         </button>
-//          </div>
-//         <div className={`col-6 ${styles.buy}`}>
-//           <p>Buy</p>
-//         </div>
-//       </div>
-//     </div>
-//         )
-// }
-
-// export default Card
-
-
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "./card.module.css";
@@ -134,8 +55,8 @@ function Card({ game, refreshHandler, handleAddList, selectedGames }) {
         idVideogame: [game.id],
         idUser: user?.sub
       };
+      console.log(requestData)
       const response = await axios.post("http://localhost:3001/payment/buy", requestData);
-      window.alert("se realizo la compra de forma exitosa")
     } catch (error) {
       window.alert(error.message)
     }
@@ -174,12 +95,14 @@ function Card({ game, refreshHandler, handleAddList, selectedGames }) {
       <div className="row">
         <div className={`col-6 ${styles.buy}`}>
           <button onClick={addDleneteFavorites}>
-            <FontAwesomeIcon
-              icon={faHeart}
-              className={
-                Favorites?.length ? styles.heartIconFav : styles.heartIcon
-              }
-            />
+          {
+                  game?.Favorites?.buy?(<p>Purched</p>):(
+                    <FontAwesomeIcon
+                    icon={faHeart}
+                    className={
+                      Favorites?.length ? styles.heartIconFav : styles.heartIcon
+                    }
+                  />)}
           </button>
         </div>
         <div className={`col-6 ${styles.buy}`}>
