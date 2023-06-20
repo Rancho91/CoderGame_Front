20d5615b0e9aed5e8ad43164c95aa193a85f4c6e
walterporta <walterandresporta@gmail.com>
2023-06-20 12:12:59 -0300

    dos card en home, una al lado de la otra

--- src/component/Home/home.jsx
+++ src/component/Home/home.jsx
@@ -1,51 +1,46 @@
 import React from "react";
-import axios from "axios"
-import Slider from "../Slider/slider"
+import axios from "axios";
 import { useEffect, useState } from "react";
 import imagenSlader from "./helper/imagenSlader";
-import SubNavBar from "../SubNavBar/SubNavBar";
-import Card from "../card/card"
-import styles from './home.module.css'
+import Card from "../card/card";
+import styles from "./home.module.css";
 import Carousel from "../Carousel/Carousel";
-import { api } from '../../App'
+import { api } from "../../App";
 
-const Home = () =>{
-    const [arrSlader, setArrSlader] = useState([])
-    const [arrGamesFooter, setArrGameFooter] = useState([])
+const Home = () => {
+  const [arrSlader, setArrSlader] = useState([]);
+  const [arrGamesFooter, setArrGameFooter] = useState([]);
 
-    useEffect(()=>{    
-        const videogames = async () => {
-            const response = await api.get("videogames")
-            return response.data
-        }
-        videogames().then((response)=>{
-            console.log(response)
-            const sliderImage= imagenSlader(response.Videogames,5)
-            setArrSlader(sliderImage)
-            const imageFooter = imagenSlader(response.Videogames,2)
-            setArrGameFooter(imageFooter)
-        })
-    },[])
+  useEffect(() => {
+    const videogames = async () => {
+      const response = await api.get("videogames");
+      return response.data;
+    };
+    videogames().then((response) => {
+      console.log(response);
+      const sliderImage = imagenSlader(response.Videogames, 5);
+      setArrSlader(sliderImage);
+      const imageFooter = imagenSlader(response.Videogames, 2);
+      setArrGameFooter(imageFooter);
+    });
+  }, []);
 
-    
-    return(
-        <div className={`container ${styles.home}`}>
-            {/* <p />
-            <SubNavBar />
-        <p /> */}
-        <div> <Carousel /> </div>
-            {/* <div className="col-12 d-flex justify-content-center">
-                <Slider image={arrSlader}/>        
-            </div> */}
-            <div className={`row d-flex justify-content-center ${styles.cardFooter}`}>
-                    {arrGamesFooter?.map((game) => (
-                    <div className={`col-md-5 col-sm-6${styles.card}`}>
-                        <Card game={game} />
-                    </div>  ))}
+  return (
+    <div className={`container ${styles.home}`}>
+      <div>
+        {" "}
+        <Carousel />{" "}
+      </div>
+
+      <div className={`row d-flex justify-content-center cardInner ${styles.cardInner}`}>
+        {arrGamesFooter?.map((game) => (
+            <div className={`col-md-5 col-6 ${styles.card}`}>
+            <Card game={game} />
+          </div>
+        ))}
+      </div>
     </div>
-</div>
- 
-        )
-}
+  );
+};
 
-export default Home
+export default Home;
